d0153cb22093e26684d61f627b6abe2f5da57138
Bilal van Loon <vanloon_1983@hotmail.com>
2024-09-26 22:42:15 +0200

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function() {
+  $title = 'Available Jobs';
+  $jobs = [
+
+  ];
+
+  return view('jobs.index', compact('title', 'jobs'));
+})->name('jobs');
+
+Route::get('/jobs/create', function() {
+  return view('jobs.create');
+})->name('jobs.create');
